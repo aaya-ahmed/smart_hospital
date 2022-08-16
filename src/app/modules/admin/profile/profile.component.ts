195b15dca7271183aa3a187ff8ee5ddc7c6eb257
aaya-ahmed <aaya7098@gmail.com>
2022-08-17 00:41:43 +0200

    new

--- src/app/modules/admin/profile/profile.component.ts
+++ src/app/modules/admin/profile/profile.component.ts
@@ -5,7 +5,7 @@ import { ServicesService } from 'src/app/services/services.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css','../../styles/profilestyle2.css']
 })
 export class ProfileComponent implements OnInit {
   admin:any;
--- src/app/modules/patient/profile/profile.component.ts
+++ src/app/modules/patient/profile/profile.component.ts
@@ -4,7 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css','../../styles/profilestyle2.css']
 })
 export class ProfileComponent implements OnInit {
   patient:any;
